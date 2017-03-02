b03f62d43e0d506e12f4e0f139da91e6e29c8adc
Josiah Gallegos <jgallegos20@my.whitworth.edu>
2017-03-01 23:04:25 -0800

    revised ex03_1

--- HW3/HW3/main.cpp
+++ HW3/HW3/main.cpp
@@ -7,13 +7,31 @@
 //
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
+#include <limits>
+#include <ios>
 using namespace std;
 
 int main(){
     return 0;
 }
 //EX03_1
-int read_int(string prompt, int smallest, int largest){
+int read_int(const string& prompt, int smallest, int largest){
+    cin.exceptoins(ios_base::failbit);
+    int num = 0;
+    while(true){
+        try{
+            cout<<prompt;
+            cin>>num;
+            return num;
+        }
+        catch(ios_base::failure& ex){
+            cout<<"Bad numeric string -- try again\n";
+            cin.clear();
+            cin.ignore(numeric_limits<int>::max(), '\n');
+        }
+    }
     if (smallest == largest){
         throw std::invalid_argument("No values in range");
     }
